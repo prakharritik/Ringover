3fda16b22929d7ebdf68c5d762c0e843dbdaf7b8
prakharritik <prakharritiksaxena@gmail.com>
2023-02-05 19:30:28 +0530

    Prepare for deploy.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { connectDB } = require("./db");
+const path = require("path");
 
 const PORT = process.env.PORT || 8000;
 
@@ -12,5 +13,12 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/product", require("./routes/api/product"));
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client", "build", "index.html"));
+  console.log(path.join(__dirname, "./client", "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Application running on PORT:${PORT}`));
